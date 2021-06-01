656d79ad0dd4489c4507ed65aefad9686e633e47
Juan E <jeduardo.garcia@gmail.com>
2021-06-01 19:21:02 -0400

    Update CountDiv.lua

--- CountDiv.lua
+++ CountDiv.lua
@@ -1,3 +1,5 @@
+--- Correct but slow
+
 function solution(A, B, K)
     -- write your code in Lua 5.3
     cant = 0
@@ -9,4 +11,23 @@ function solution(A, B, K)
     end
 
     return cant
+end
+
+--- Fast but wrong
+function solution(A, B, K)
+    -- write your code in Lua 5.3
+    cont = 0
+    if (A % K == 0 ) then
+       cont = cont + 1
+    end
+
+    if (B % K == 0 ) then
+       cont = cont + 1
+    end
+
+    --print (math.modf( (B-A)/K ))
+    cont = cont + math.modf( (B-A)/K )
+    --print(cont)
+    return cont
+
 end
